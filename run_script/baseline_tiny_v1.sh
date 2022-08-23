2918854a1aae7529c91155c62d5d1c286cfa40a7
zhou745 <zhoujq931118@gmail.com>
2022-08-23 10:29:18 +0800

    add one more script

--- run_script/baseline_tiny_v1.sh
+++ run_script/baseline_tiny_v1.sh
@@ -1,2 +1,2 @@
 python -m torch.distributed.launch --nproc_per_node=8 --use_env main.py --dist-eval --model deit_tiny_patch16_224 --batch-size 256\
- --data-path ./datasets/imagenet --output_dir ./training/baseline_tiny_v1 --epochs 300 --lr 5e-4
+ --data-path ~/datasets/imagenet --output_dir ./training/baseline_tiny_v1 --epochs 300 --lr 5e-4
